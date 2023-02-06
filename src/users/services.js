7abcbdbe04984cebabc35b47a60ebe52a5a04baa
RABrL <robin_brito@hotmail.com>
2023-02-06 00:00:43 -0500

    Modify updtae method

--- src/users/services.js
+++ src/users/services.js
@@ -23,9 +23,9 @@ const create = async (user) => {
   return result.insertedId
 }
 
-const update = async (id, product) => {
+const update = async (id, user) => {
   const collection = await Database(COLLECTION)
-  const result = await collection.replaceOne({ _id: new ObjectId(id) }, product)
+  const result = await collection.replaceOne({ _id: new ObjectId(id) }, user)
   return result.matchedCount
 }
 
